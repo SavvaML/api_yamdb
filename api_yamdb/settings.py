9815fa2dc132a09192629cfa52d36d0c5dc9835c
Savva <savva.manylov@bk.ru>
2020-10-14 14:59:07 +0300

    1s

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -31,20 +31,17 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'api',
+    'users',
+    'titles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Third-Party Apps
     'rest_framework',
     'django_filters',
-    # Local Apps
-    'api',
-    #'users',
-    'titles',
-    'users.apps.UsersConfig',
 
 ]
 AUTH_USER_MODEL = 'users.Users'
